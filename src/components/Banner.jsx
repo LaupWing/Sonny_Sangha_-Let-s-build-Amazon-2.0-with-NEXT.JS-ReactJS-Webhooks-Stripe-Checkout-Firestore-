de2bf56490afa478ad886af3adf3ae970417fa5d
LaupWing <loc_strict@hotmail.com>
2022-11-06 17:26:11 +0100

    update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 
 const Banner = () => {
    return (
@@ -13,7 +14,7 @@ const Banner = () => {
             interval={5000}
          >
             <div>
-               <img loading="lazy" src="https://links.papareact.com/gil" alt="" />
+               <img loading="lazy" src="https://links.papareact.com/gi1" alt="" />
             </div>
             <div>
                <img loading="lazy" src="https://links.papareact.com/6ff" alt="" />
