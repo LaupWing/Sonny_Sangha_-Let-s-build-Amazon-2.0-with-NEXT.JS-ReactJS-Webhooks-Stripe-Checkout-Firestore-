ebd4ca5e47c5629957218902c3221bad44839f66
LaupWing <loc_strict@hotmail.com>
2022-11-06 17:23:25 +0100

    udpate

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Carousel } from "react-responsive-carousel"
 
 const Banner = () => {
    return (
-      <div>Banner</div>
+      <div className="relative">
+         <Carousel
+            autoPlay
+            infiniteLoop
+            showStatus={false}
+            showIndicators={false}
+            showThumbs={false}
+            interval={5000}
+         >
+            <div>
+               <img loading="lazy" src="https://links.papareact.com/gil" alt="" />
+            </div>
+            <div>
+               <img loading="lazy" src="https://links.papareact.com/6ff" alt="" />
+            </div>
+            <div>
+               <img loading="lazy" src="https://links.papareact.com/7ma" alt="" />
+            </div>
+         </Carousel>
+      </div>
    )
 }
 
