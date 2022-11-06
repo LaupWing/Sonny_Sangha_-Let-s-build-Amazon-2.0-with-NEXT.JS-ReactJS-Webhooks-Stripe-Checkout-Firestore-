40ee93a4348be87c0c1d14f82c6fff65a9b8ed96
LaupWing <loc_strict@hotmail.com>
2022-11-06 17:47:29 +0100

    update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { StarIcon } from "@heroicons/react/24/outline"
+import Image from "next/image"
+import React, { useState } from 'react'
 
 const Product = ({ id, title, price, description, category, image }) => {
+   const [rating] = useState(2)
+
    return (
-      <div>Product</div>
+      <div>
+         <p>{category}</p>
+         <Image
+            src={image}
+            height={200}
+            width={200}
+            objectFit="contain"
+         />
+         <div className="flex">
+            {Array(rating)
+               .fill()
+               .map((_, i) => (
+                  <StarIcon
+                     key={i}
+                     className="h-5"
+                  />
+               ))}
+         </div>
+         <h4>{title}</h4>
+      </div>
    )
 }
 
