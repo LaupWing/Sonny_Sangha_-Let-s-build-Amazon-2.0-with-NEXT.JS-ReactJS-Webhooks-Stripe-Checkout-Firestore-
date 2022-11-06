ebc5bb303589d589b15eb9636d80021f45868c98
LaupWing <loc_strict@hotmail.com>
2022-11-06 17:19:29 +0100

    react responsive carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -7,7 +8,12 @@ export default function Home() {
          <Head>
             <title>Amazon 2.0</title>
          </Head>
-         <Header/>
+         <Header />
+
+         <main className="max-w-screen-2xl mx-auto">
+            <Banner />
+            {/* ProductFeed */}
+         </main>
       </div>
    );
 }
