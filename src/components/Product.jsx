de9c1f3f406c1b3b602071a54f168b1342e0457d
LaupWing <loc_strict@hotmail.com>
2022-11-11 19:27:27 +0100

    update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,7 @@
 import { StarIcon } from "@heroicons/react/24/outline"
 import Image from "next/image"
 import React, { useState } from 'react'
+import Currency from "react-currency-format"
 
 const MAX_RATING = 5
 const MIN_RATING = 1
@@ -11,15 +12,15 @@ const Product = ({ id, title, price, description, category, image }) => {
    const [hasPrime] = useState(Math.random() < 0.5)
 
    return (
-      <div>
-         <p>{category}</p>
+      <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+         <p className="absolute top-2 right-2 text-xs italic">{category}</p>
          <Image
             src={image}
             height={200}
             width={200}
             objectFit="contain"
          />
-         <h4>{title}</h4>
+         <h4 className="my-3">{title}</h4>
          <div className="flex">
             {Array(rating)
                .fill()
@@ -32,11 +33,19 @@ const Product = ({ id, title, price, description, category, image }) => {
          </div>
 
          {hasPrime && <p>Has prime delivery</p>}
-         <p>{description}</p>
+         <p className="text-xs my-2 line-clamp-2">{description}</p>
 
-         <div>
-
+         <div className="mb-5">
+            <p>$ {price}</p>
          </div>
+         {hasPrime && (
+            <div className="flex items-center space-x-2 -mt-5">
+               <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
+               <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+            </div>
+         )}
+
+         <button className="mt-auto button">Add To Basket</button>
       </div>
    )
 }
